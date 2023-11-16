93d494e64985b0afbcbed8bdfda8a48394a876bd
nda666 <adhabakhtiar@gmail.com>
2023-11-16 16:35:07 +0700

    fix migration

--- prisma/migrations/20231113031036_add_id_table_participants_and_alter_fk_participant_id_groupsonparticipants/migration.sql
+++ prisma/migrations/20231113031036_add_id_table_participants_and_alter_fk_participant_id_groupsonparticipants/migration.sql
@@ -1,24 +1,34 @@
 /*
-  Warnings:
-
-  - The primary key for the `groupsonparticipants` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to alter the column `participantId` on the `groupsonparticipants` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
-  - The primary key for the `participants` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to alter the column `id` on the `participants` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
-
-*/
+ Warnings:
+ 
+ - The primary key for the `groupsonparticipants` table will be changed. If it partially fails, the table could be left without primary key constraint.
+ - You are about to alter the column `participantId` on the `groupsonparticipants` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+ - The primary key for the `participants` table will be changed. If it partially fails, the table could be left without primary key constraint.
+ - You are about to alter the column `id` on the `participants` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+ 
+ */
 -- DropForeignKey
-ALTER TABLE `groupsonparticipants` DROP FOREIGN KEY `GroupsOnParticipants_participantId_fkey`;
+ALTER TABLE
+  `GroupsOnParticipants` DROP FOREIGN KEY `GroupsOnParticipants_participantId_fkey`;
 
 -- AlterTable
-ALTER TABLE `groupsonparticipants` DROP PRIMARY KEY,
-    MODIFY `participantId` INTEGER NOT NULL,
-    ADD PRIMARY KEY (`participantId`, `groupId`);
+ALTER TABLE
+  `GroupsOnParticipants` DROP PRIMARY KEY,
+MODIFY
+  `participantId` INTEGER NOT NULL,
+ADD
+  PRIMARY KEY (`participantId`, `groupId`);
 
 -- AlterTable
-ALTER TABLE `participants` DROP PRIMARY KEY,
-    MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT,
-    ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `participants` DROP PRIMARY KEY,
+MODIFY
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+ADD
+  PRIMARY KEY (`id`);
 
 -- AddForeignKey
-ALTER TABLE `GroupsOnParticipants` ADD CONSTRAINT `GroupsOnParticipants_participantId_fkey` FOREIGN KEY (`participantId`) REFERENCES `participants`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  `GroupsOnParticipants`
+ADD
+  CONSTRAINT `GroupsOnParticipants_participantId_fkey` FOREIGN KEY (`participantId`) REFERENCES `participants`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20231113061452_alter_add_id_column_groups_on_participant_table/migration.sql
+++ prisma/migrations/20231113061452_alter_add_id_column_groups_on_participant_table/migration.sql
@@ -1,29 +1,36 @@
 /*
-  Warnings:
-
-  - You are about to drop the `groupsonparticipants` table. If the table is not empty, all the data it contains will be lost.
-
-*/
+ Warnings:
+ 
+ - You are about to drop the `groupsonparticipants` table. If the table is not empty, all the data it contains will be lost.
+ 
+ */
 -- DropForeignKey
-ALTER TABLE `groupsonparticipants` DROP FOREIGN KEY `GroupsOnParticipants_groupId_fkey`;
+ALTER TABLE
+  `GroupsOnParticipants` DROP FOREIGN KEY `GroupsOnParticipants_groupId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `groupsonparticipants` DROP FOREIGN KEY `GroupsOnParticipants_participantId_fkey`;
+ALTER TABLE
+  `GroupsOnParticipants` DROP FOREIGN KEY `GroupsOnParticipants_participantId_fkey`;
 
 -- DropTable
-DROP TABLE `groupsonparticipants`;
+DROP TABLE `GroupsOnParticipants`;
 
 -- CreateTable
 CREATE TABLE `groups_on_participants` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `participantId` INTEGER NOT NULL,
-    `groupId` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `participantId` INTEGER NOT NULL,
+  `groupId` VARCHAR(191) NOT NULL,
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `groups_on_participants` ADD CONSTRAINT `groups_on_participants_participantId_fkey` FOREIGN KEY (`participantId`) REFERENCES `participants`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  `groups_on_participants`
+ADD
+  CONSTRAINT `groups_on_participants_participantId_fkey` FOREIGN KEY (`participantId`) REFERENCES `participants`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `groups_on_participants` ADD CONSTRAINT `groups_on_participants_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  `groups_on_participants`
+ADD
+  CONSTRAINT `groups_on_participants_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
