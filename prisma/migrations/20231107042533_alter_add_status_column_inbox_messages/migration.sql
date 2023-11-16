5e92b5d2fff3b5bd37f91c4a649af0dba550e2ef
nda666 <adhabakhtiar@gmail.com>
2023-11-16 16:39:34 +0700

    add keterangan

--- prisma/migrations/20231107042533_alter_add_status_column_inbox_messages/migration.sql
+++ prisma/migrations/20231107042533_alter_add_status_column_inbox_messages/migration.sql
@@ -1,3 +1,12 @@
 -- AlterTable
-ALTER TABLE `inbox_messages` ADD COLUMN `status` INTEGER NULL,
-    MODIFY `quote` TEXT NULL;
+ALTER TABLE
+    `inbox_messages`
+ADD
+    COLUMN `status` INTEGER NULL,
+MODIFY
+    `quote` TEXT NULL;
+
+ALTER TABLE
+    `inbox_messages`
+ADD
+    COLUMN `keterangan` TEXT NULL;
