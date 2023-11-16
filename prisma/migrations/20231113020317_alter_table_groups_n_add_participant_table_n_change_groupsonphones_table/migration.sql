70bf85882a158552f71a46fce068c8d66ea14c66
nda666 <adhabakhtiar@gmail.com>
2023-11-16 16:31:18 +0700

    fix GroupsOnPhones

--- prisma/migrations/20231113020317_alter_table_groups_n_add_participant_table_n_change_groupsonphones_table/migration.sql
+++ prisma/migrations/20231113020317_alter_table_groups_n_add_participant_table_n_change_groupsonphones_table/migration.sql
@@ -1,36 +1,42 @@
 /*
-  Warnings:
-
-  - You are about to drop the `groupsonphones` table. If the table is not empty, all the data it contains will be lost.
-
-*/
+ Warnings:
+ 
+ - You are about to drop the `groupsonphones` table. If the table is not empty, all the data it contains will be lost.
+ 
+ */
 -- DropForeignKey
-ALTER TABLE `groupsonphones` DROP FOREIGN KEY `GroupsOnPhones_groupId_fkey`;
+ALTER TABLE
+  `GroupsOnPhones` DROP FOREIGN KEY `GroupsOnPhones_groupId_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `groupsonphones` DROP FOREIGN KEY `GroupsOnPhones_phoneId_fkey`;
+ALTER TABLE
+  `GroupsOnPhones` DROP FOREIGN KEY `GroupsOnPhones_phoneId_fkey`;
 
 -- DropTable
-DROP TABLE `groupsonphones`;
+DROP TABLE `GroupsOnPhones`;
 
 -- CreateTable
 CREATE TABLE `participants` (
-    `id` VARCHAR(191) NOT NULL,
-    `admin` VARCHAR(191) NULL,
-
-    PRIMARY KEY (`id`)
+  `id` VARCHAR(191) NOT NULL,
+  `admin` VARCHAR(191) NULL,
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `GroupsOnParticipants` (
-    `participantId` VARCHAR(191) NOT NULL,
-    `groupId` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`participantId`, `groupId`)
+  `participantId` VARCHAR(191) NOT NULL,
+  `groupId` VARCHAR(191) NOT NULL,
+  PRIMARY KEY (`participantId`, `groupId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `GroupsOnParticipants` ADD CONSTRAINT `GroupsOnParticipants_participantId_fkey` FOREIGN KEY (`participantId`) REFERENCES `participants`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  `GroupsOnParticipants`
+ADD
+  CONSTRAINT `GroupsOnParticipants_participantId_fkey` FOREIGN KEY (`participantId`) REFERENCES `participants`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `GroupsOnParticipants` ADD CONSTRAINT `GroupsOnParticipants_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE
+  `GroupsOnParticipants`
+ADD
+  CONSTRAINT `GroupsOnParticipants_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
