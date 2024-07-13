3966b42d9de593a2b338d766975fab5e3fa0d84d
nda666 <adhabakhtiar@gmail.com>
2024-07-13 10:31:16 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      
       name: "doran-whatsapp",
       script: "./dist/src/server.js",
       env: { NODE_ENV: "production" },
+      node_args: ["--max-old-space-size=4096"],
     },
   ],
 };
