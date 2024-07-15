33d05d1f3e509e56928d2a31e3252a2e1aa4acea
nda666 <adhabakhtiar@gmail.com>
2024-07-15 13:08:30 +0700

    max-old-space-size

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "doran-whatsapp",
       script: "./dist/src/server.js",
       env: { NODE_ENV: "production" },
-      // node_args: ["--max-old-space-size=4096"],
+      node_args: ["--max-old-space-size=1024"],
       max_memory_restart: "600M",
     },
   ],
