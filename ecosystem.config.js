39dc41c1d7ce45ca39ce89bce5e26fa9d4a67eea
nda666 <adhabakhtiar@gmail.com>
2024-10-28 13:34:31 +0700

    small fix 3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,12 @@ module.exports = {
     {
       name: "doran-whatsapp",
       script: "./dist/src/server.js",
-      env: { NODE_ENV: "production" },
+      env: {
+        NODE_ENV: "production",
+        WHATSAPP_AUTH_FOLDER: "./storage/whatsapp-auth",
+        WHATSAPP_LOG: "./storage/logs/whatsapp-logs",
+        WEBSITE_LOG: "./storage/logs/website",
+      },
       node_args: ["--max-old-space-size=1024"],
       max_memory_restart: "1000M",
     },
