536a5740d9e1acf2e5b668478408bcd31b8f8930
nda666 <adhabakhtiar@gmail.com>
2024-10-30 10:30:25 +0700

    no memory restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,9 @@ module.exports = {
         WHATSAPP_LOG: "./storage/logs/whatsapp-logs",
         WEBSITE_LOG: "./storage/logs/website",
       },
-      node_args: ["--max-old-space-size=1024"],
-      max_memory_restart: "1000M",
+      node_args: ["--max-old-space-size=4096"],
+      // node_args: ["--max-old-space-size=1024"],
+      // max_memory_restart: "2000M",
     },
   ],
 };
