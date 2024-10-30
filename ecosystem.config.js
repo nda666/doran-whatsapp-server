ba8c8b0d2243bc647aae3e1b2d7b3e4981ccd145
nda666 <adhabakhtiar@gmail.com>
2024-10-30 12:42:29 +0700

    try cluster mode #2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         WHATSAPP_LOG: "./storage/logs/whatsapp-logs",
         WEBSITE_LOG: "./storage/logs/website",
       },
-      node_args: ["--max-old-space-size=4096"],
+      node_args: ["--max-old-space-size=2048"],
       // node_args: ["--max-old-space-size=1024"],
       // max_memory_restart: "2000M",
     },
